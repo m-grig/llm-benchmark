8be80e06854509f9d5c0dd8e515cda237a7678f6
m-grig <charril314@gmail.com>
2025-08-13 23:31:43 -0600

    Improve tests

--- src/tests/math.ts
+++ src/tests/math.ts
@@ -2,32 +2,33 @@ import type { LLM } from '@lmstudio/sdk';
 import { scoreArray } from '../util/array';
 
 export async function runMathTests(model: LLM) {
-  console.log('Running math tests')
-  const scores = tests.map(async (test) => {
-    const response = await model.respond(test.prompt);
-    return test.validation(response.nonReasoningContent);
-  });
-  const finalScore = scoreArray(await Promise.all(scores));
-  console.log('Math tests:');
-  console.log(`${finalScore.percentage.toFixed(2)}% ${finalScore.score} out of ${finalScore.total}`);
+  console.log('🧠 Running math tests...');
+  await testBasicAlgebra(model);
 }
 
-type MathTest = {
-  prompt: string;
-  validation: (value: any) => number;
-};
-
-const tests: MathTest[] = [
-  {
-    prompt: `Solve this algebra equation for x. Only respond with the correct number.
-5.9 = x + 5.11`,
-    validation: (v: string) => {
-      return Number(v) === 0.79 ? 1 : 0;
-    },
-  },
-];
-
-async function basicAlgebra() {
-  const prompt = `Solve this algebra equation for x. Only respond with the correct number.
-5.9 = x + 5.11`;
+async function testBasicAlgebra(model: LLM) {
+  const instructions = 'Solve this algebra equation for x. Only respond with the correct number.\n';
+  const equations = [
+    ['3x = 21', 7],
+    ['5.9 = x + 5.11', 0.79],
+    ['x + 5 = 13', 8],
+    ['2x - 7 = 11', 9],
+    ['5x + 4 = 29', 5],
+    ['2x + 3x = 25', 5],
+  ];
+  const results = equations.map(async (equation, i) => {
+    const prompt = instructions + equation[0];
+    const response = await model.respond(prompt);
+    const result = Number(response.nonReasoningContent);
+    const score = result === equation[1] ? 1 : 0;
+    console.log(`${i + 1}/${equations.length} ${score == 1 ? '✅' : '❌'} ${equation[0]}`);
+    return { score, tokens: response.stats.totalTokensCount };
+  });
+  const awaitedResults = await Promise.all(results);
+  const finalScore = scoreArray(awaitedResults);
+  console.log('Algebra score:');
+  console.log(`${finalScore.percentage.toFixed(2)}% ${finalScore.score} out of ${finalScore.total}`);
+  console.log(
+    `Total tokens used: ${finalScore.totalTokens}; Average tokens per test: ${finalScore.averageTokensPerTest}`
+  );
 }
--- src/util/array.ts
+++ src/util/array.ts
@@ -6,12 +6,15 @@ export function sumOfArray(array: number[]) {
   return array.reduce((partialSum, a) => partialSum + a, 0);
 }
 
-export function scoreArray(array: number[]) {
-  const sum = sumOfArray(array);
-  const percentage = sum / array.length;
+export function scoreArray(array: { score: number; tokens?: number }[]) {
+  const sum = sumOfArray(array.map((val) => val.score));
+  const percentage = (sum / array.length) * 100;
+  const tokensArray = array.map((val) => val.tokens || 0);
   return {
     score: sum,
     total: array.length,
     percentage,
+    totalTokens: sumOfArray(tokensArray),
+    averageTokensPerTest: averageOfArray(tokensArray),
   };
 }
