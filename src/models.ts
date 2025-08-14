3df07ddde1b4dff233ecfd9c10133e18aa0047ff
m-grig <charril314@gmail.com>
2025-08-13 23:38:34 -0600

    Add back additional models

--- src/models.ts
+++ src/models.ts
@@ -1,3 +1,8 @@
 export enum Models {
   GEMMA_12B = 'google/gemma-3-12b',
+  QWEN3_4B_THINKING_2507 = 'qwen3-4b-thinking-2507-mlx',
+  QWEN3_8B_R1_0528_DISTILL = 'deepseek/deepseek-r1-0528-qwen3-8b',
+  LLAMA4_DOLPHIN_8B = 'llama4-dolphin-8b',
+  JAV_V1_4B = 'jan-v1-4b',
+  LFM2_1_2B = 'liquid/lfm2-1.2b',
 }
